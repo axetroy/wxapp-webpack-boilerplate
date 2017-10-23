6c71bf8a362922c4a3194cef20fa2794204c34ae
axetroy <troy450409405@gmail.com>
2017-10-23 15:33:59 +0800

    chore: Update

--- scripts/Builder.js
+++ scripts/Builder.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs-extra');
+const pd = require('pretty-data').pd;
 const CONFIG = require('./config');
 
 class Builder {
@@ -20,10 +21,22 @@ class Builder {
     const files = Object.keys(this.files);
     while (files.length) {
       let file = files.shift();
+      let fileContent = await fs.readFile(file, 'utf8');
+
+      // 生产环境下，会对一些文件进行压缩
+      if (CONFIG.isProduction) {
+        const pathInfo = path.parse(file);
+        switch (pathInfo.ext) {
+          case '.json':
+            fileContent = pd.jsonmin(fileContent);
+            break;
+        }
+      }
+
       const relativeFilePath = path.relative(CONFIG.paths.src, file);
       const distFilePath = path.join(CONFIG.paths.dist, relativeFilePath);
       await fs.ensureFile(distFilePath);
-      await fs.writeFile(distFilePath, await fs.readFile(file, 'utf8'), 'utf8');
+      await fs.writeFile(distFilePath, fileContent, 'utf8');
     }
   }
 }
--- scripts/build-xml.js
+++ scripts/build-xml.js
@@ -4,6 +4,7 @@
 
 const path = require('path');
 const fs = require('fs-extra');
+const pd = require('pretty-data').pd;
 const Builder = require('./Builder');
 const CONFIG = require('./config');
 
@@ -16,18 +17,23 @@ class XmlBuilder extends Builder {
     try {
       while (files.length) {
         let file = files.shift();
+        let fileContent = await fs.readFile(file, 'utf8');
+
+        // 生产环境下，对xml文件进行压缩
+        if (CONFIG.isProduction) {
+          fileContent = pd.xmlmin(fileContent, true);
+        }
+
         const relativeFilePath = path.relative(CONFIG.paths.src, file);
         const distFilePath = path
           .join(CONFIG.paths.dist, relativeFilePath)
           .replace(/\.w?xml$/, '.wxml');
+
         await fs.ensureFile(distFilePath);
-        await fs.writeFile(
-          distFilePath,
-          await fs.readFile(file, 'utf8'),
-          'utf8'
-        );
+        await fs.writeFile(distFilePath, fileContent, 'utf8');
       }
     } catch (err) {
+      console.error(`Compile xml fail!`);
       console.error(err);
     }
   }
